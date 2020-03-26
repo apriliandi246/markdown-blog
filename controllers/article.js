6ddae4858df6c32a8276d3fae1f490db6c5adccf
apriliandi246 <apriliandi642@gmail.com>
2020-03-26 15:52:00 +0700

    make form new article

--- controllers/article.js
+++ controllers/article.js
@@ -1,3 +1,8 @@
 module.exports.render_new_article_page = (req, res) => {
       res.render('article/new_article');
 }
+
+
+module.exports.render_detail_aticle_page = (req, res) => {
+      res.render('article/detail');
+}
--- routes/article.js
+++ routes/article.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const {
-      render_new_article_page
+      render_new_article_page,
+      render_detail_aticle_page
 } = require('../controllers/article');
 const router = express.Router();
 
 
-// GET
+// GET : new article
 router.get('/new', render_new_article_page);
+
+
+// GET : detail article
+router.get('/detail', render_detail_aticle_page);
 
 
 module.exports = router;
