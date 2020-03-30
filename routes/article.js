8dee26d71af5855e8b6bfddda852dbb6a585bae9
apriliandi246 <apriliandi642@gmail.com>
2020-03-30 12:50:33 +0700

    factory code

--- routes/article.js
+++ routes/article.js
@@ -36,11 +36,16 @@ router.post('/new', (req, res, next) => {
 }, saveArticleAndRedirect());
 
 
+// DELETE : article
+router.delete('/delete/:id', async (req, res) => {
+     await Article.findByIdAndDelete(req.params.id)
+     res.redirect('/');
+});
+
 
 function saveArticleAndRedirect() {
      return async (req, res) => {
           let article = req.article;
-
           article.title = req.body.title;
           article.description = req.body.description;
           article.markdown = req.body.markdown;
