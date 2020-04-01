8f2c670bd9aef467101bebbaacbc712c62f7d845
apriliandi246 <apriliandi642@gmail.com>
2020-04-01 13:48:22 +0700

    update UI

--- startup/main.js
+++ startup/main.js
@@ -3,19 +3,19 @@ const methodOverride = require('method-override');
 
 
 module.exports = function (app) {
+      app.disable('etag');
+      app.disable('x-powered-by');
+
       app.use(express.urlencoded({
             extended: false
       }));
       app.use(methodOverride('_method'));
 
-      app.disable('etag');
-      app.disable('x-powered-by');
-
       //routes
       app.use('/', require('../routes/index'));
       app.use('/article', require('../routes/article'));
 
-      app.use(function (req, res, next) {
+      app.use((req, res, next) => {
             res.redirect('/');
       });
 }
