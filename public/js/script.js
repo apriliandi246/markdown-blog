2e9a09956b2648f3b3b364ac469eacd2968881a4
apriliandi <apriliandi642@gmail.com>
2020-07-14 03:21:02 +0700

    add dropdown to detail article

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
+// for styling code
 document.addEventListener('DOMContentLoaded', () => {
    document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
    });
 });
 
+
+// make link tag target blank
 const link = document.querySelectorAll('.article-body a');
 
 for (let i = 0; i < link.length - 1; i++) {
