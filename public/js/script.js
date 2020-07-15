6f868a0ebd02f993836ae9ab06c298a736daf218
apriliandi <apriliandi642@gmail.com>
2020-07-16 00:37:17 +0700

    add feature delete article and factory some code

--- public/js/script.js
+++ public/js/script.js
@@ -14,8 +14,15 @@ for (let i = 0; i < link.length - 1; i++) {
 }
 
 
-// delete article menu
-document.querySelector('nav .dropdown .dropdown-content a.delete-article')
-   .addEventListener('click', () => {
-      alert("Coming Soon 😅😅😅😅");
-   });
+// delete article menu and make modal
+const modal = document.querySelector('.modal-delete');
+
+// show the modal
+document.querySelector('nav .dropdown .dropdown-content button.delete-article').addEventListener('click', () => {
+   modal.style.display = 'block';
+});
+
+// hide the modal
+document.querySelector('.modal-delete .modal .modal-footer button.cancel-btn').addEventListener('click', () => {
+   modal.style.display = 'none';
+});
