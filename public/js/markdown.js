6e068b071cffb898c0f805994c4be569701b636b
apriliandi <apriliandi642@gmail.com>
2020-08-16 22:37:05 +0700

    chnage event listener

--- public/js/markdown.js
+++ public/js/markdown.js
@@ -1,14 +1,12 @@
-// for styling code
+// for code format
 document.addEventListener('DOMContentLoaded', () => {
+   const anchorTag = document.querySelectorAll('.article-body a');
+
+   for (let i = 0; i < anchorTag.length - 1; i++) {
+      anchorTag[i].target = '_blank';
+   }
+
    document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
    });
 });
-
-
-// make link tag target blank
-const link = document.querySelectorAll('.article-body a');
-
-for (let i = 0; i < link.length - 1; i++) {
-   link[i].target = '_blank';
-}
