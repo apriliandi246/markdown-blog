533e4760136af5ad58bfd91b71ed3e0e3f3c2bba
apriliandi <apriliandi642@gmail.com>
2020-08-17 17:29:47 +0700

    factory dom manipulation code

--- public/js/markdown.js
+++ public/js/markdown.js
@@ -1,10 +1,7 @@
-// for code format
 document.addEventListener('DOMContentLoaded', () => {
-   const anchorTag = document.querySelectorAll('.article-body a');
-
-   for (let i = 0; i < anchorTag.length - 1; i++) {
-      anchorTag[i].target = '_blank';
-   }
+   document.querySelectorAll('.article-body a').forEach((anchorTag) => {
+      anchorTag.target = '_blank';
+   });
 
    document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
