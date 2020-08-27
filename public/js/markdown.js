22332740a12b139a4ab1f9872a473645865dd2fe
apriliandi <apriliandi642@gmail.com>
2020-08-27 16:23:12 +0700

    factory javascript dom code

--- public/js/markdown.js
+++ public/js/markdown.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-   document.querySelectorAll('.article-body a').forEach((anchorTag) => {
-      anchorTag.target = '_blank';
-   });
+   const links = document.querySelectorAll('.article-body a');
+
+   for (let index = 0; index < links.length; index++) {
+      links[index].target = '_blank';
+   }
 
    document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightBlock(block);
