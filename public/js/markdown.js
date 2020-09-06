d7d1acc02ac0b74c19f949e270d6e617049c043c
apriliandi <apriliandi642@gmail.com>
2020-09-07 04:03:33 +0700

    handle table element for responsive mode

--- public/js/markdown.js
+++ public/js/markdown.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-   const links = document.querySelectorAll('.article-body a');
+   const tables = document.getElementsByTagName('table');
+   const parent = document.querySelector('.article__body');
 
-   for (let index = 0; index < links.length; index++) {
-      links[index].target = '_blank';
+   for (let index = 0; index < tables.length; index++) {
+      const parentTable = document.createElement('div');
+      const nextEl = tables[index].nextElementSibling;
+
+      parentTable.classList.add('table-body');
+      parentTable.style.marginTop = '50px';
+      parentTable.append(tables[index]);
+
+      parent.insertBefore(parentTable, nextEl);
    }
 
    document.querySelectorAll('pre code').forEach((block) => {
